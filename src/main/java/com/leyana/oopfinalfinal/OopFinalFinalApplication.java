81d3a1574abaa807c25f1c2b322012d83622a5c1
leyanadabare <leyanadabare1@gmail.com>
2024-11-29 12:50:10 +0530

    initial commit

---
+++ src/main/java/com/leyana/oopfinalfinal/OopFinalFinalApplication.java
@@ -0,0 +1,13 @@
+package com.leyana.oopfinalfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopFinalFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopFinalFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leyana/oopfinalfinal/OopFinalFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leyana.oopfinalfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopFinalFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
