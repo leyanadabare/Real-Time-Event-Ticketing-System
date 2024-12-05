cb75210f02c6fb958ed42dea59b6ea8d7cdd9ffa
leyanadabare <leyanadabare1@gmail.com>
2024-12-05 15:45:20 +0530

    refactor: Moved the Main class to util package.

---
+++ src/main/java/util/Main.java
@@ -0,0 +1,45 @@
+package util;
+
+import objects.Customer;
+import objects.TicketPool;
+import objects.Vendor;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import static util.Services.initializeTicketingSystem;
+import static util.Services.loadConfiguration;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("...Welcome to the Ticketing System Application!...");
+        Scanner scanner = new Scanner(System.in);
+        Configuration config = new Configuration();
+
+        try {
+            Logging.log("INFO", "Starting the Ticketing System Application.");
+            loadConfiguration(scanner, config);
+            initializeTicketingSystem(config);
+        } catch (ConfigurationLoadException | InputValidationException e) {
+            Logging.log("ERROR", "Error occurred: " + e.getMessage());
+        } finally {
+            Logging.log("INFO", "Shutting down the Ticketing System application.");
+            Logging.close();
+        }
+    }
+
+    class ConfigurationLoadException extends Exception {
+        public ConfigurationLoadException(String message) {
+            super(message);
+        }
+    }
+
+    static class InputValidationException extends Exception {
+        public InputValidationException(String message) {
+            super(message);
+        }
+    }
+
+
+}
