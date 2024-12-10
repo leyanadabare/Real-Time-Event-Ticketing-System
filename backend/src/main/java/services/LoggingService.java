cc01cf28cc535bd1cfe008f19b2ddaa22f8ee928
leyanadabare <leyanadabare1@gmail.com>
2024-12-10 23:24:41 +0530

    feat: adding API's to Logging

---
+++ backend/src/main/java/services/LoggingService.java
@@ -0,0 +1,27 @@
+package services;
+import org.springframework.stereotype.Service;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class LoggingService {
+
+    private static final String LOG_FILE_NAME = "/Users/leyanadaba/Desktop/OOPFinalFinal/ticketing_system.log";
+
+    public List<String> getLogs() {
+        List<String> logs = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(LOG_FILE_NAME))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                logs.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return logs;
+    }
+}
+
